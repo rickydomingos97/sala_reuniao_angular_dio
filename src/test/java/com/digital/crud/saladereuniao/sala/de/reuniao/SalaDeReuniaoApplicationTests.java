26693307ed6200526f6d8a8fe613e9606a746dac
rickydomingos97 <ricardodomingos97@gmail.com>
2020-12-11 13:14:43 -0300

    initial commit

---
+++ src/test/java/com/digital/crud/saladereuniao/sala/de/reuniao/SalaDeReuniaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.crud.saladereuniao.sala.de.reuniao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaDeReuniaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
